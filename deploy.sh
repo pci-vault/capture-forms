22864b096712f048bddf3020588e5350d93ea770
Arie De Bok <arie@directdebit.co.za>
2023-09-27 15:53:16 +0200

    Fix wrong s3 directory

--- deploy.sh
+++ deploy.sh
@@ -22,16 +22,16 @@ aws s3 cp public/build/ach_form.js s3://pvs-frontend/"$stage"/ach/ --acl public-
 aws s3 cp public/build/ach_form.css s3://pvs-frontend/"$stage"/ach/ --acl public-read
 
 echo "--- UPLOADING SHARED ASSETS ---"
-aws s3 cp public/assets/card.svg s3://pvs-frontend/assets/"$stage"/ --acl public-read
-aws s3 cp public/assets/high-summit.regular.ttf s3://pvs-frontend/assets/"$stage"/ --acl public-read
-aws s3 cp public/assets/SFProDisplay-Bold.woff2 s3://pvs-frontend/assets/"$stage"/ --acl public-read
-aws s3 cp public/assets/SFProDisplay-Light.woff2 s3://pvs-frontend/assets/"$stage"/ --acl public-read
-aws s3 cp public/assets/SFProDisplay-Medium.woff2 s3://pvs-frontend/assets/"$stage"/ --acl public-read
-aws s3 cp public/assets/SFProDisplay-Regular.woff2 s3://pvs-frontend/assets/"$stage"/ --acl public-read
-aws s3 cp public/assets/logo.webp s3://pvs-frontend/assets/"$stage"/ --acl public-read
-aws s3 cp public/assets/chip.svg s3://pvs-frontend/assets/"$stage"/ --acl public-read
+aws s3 cp public/assets/card.svg s3://pvs-frontend/"$stage"/assets/ --acl public-read
+aws s3 cp public/assets/high-summit.regular.ttf s3://pvs-frontend/"$stage"/assets/ --acl public-read
+aws s3 cp public/assets/SFProDisplay-Bold.woff2 s3://pvs-frontend/"$stage"/assets/ --acl public-read
+aws s3 cp public/assets/SFProDisplay-Light.woff2 s3://pvs-frontend/"$stage"/assets/ --acl public-read
+aws s3 cp public/assets/SFProDisplay-Medium.woff2 s3://pvs-frontend/"$stage"/assets/ --acl public-read
+aws s3 cp public/assets/SFProDisplay-Regular.woff2 s3://pvs-frontend/"$stage"/assets/ --acl public-read
+aws s3 cp public/assets/logo.webp s3://pvs-frontend/"$stage"/assets/ --acl public-read
+aws s3 cp public/assets/chip.svg s3://pvs-frontend/"$stage"/assets/ --acl public-read
 
 for file in public/assets/card-icons/*
 do
-  aws s3 cp "$file" s3://pvs-frontend/assets/"$stage"/card-icons/ --acl public-read
+  aws s3 cp "$file" s3://pvs-frontend/"$stage"/assets/card-icons/ --acl public-read
 done
