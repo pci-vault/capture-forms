b03169733e3c8c1e0a232cac19ffe6baced53c56
Arie De Bok <arie@directdebit.co.za>
2023-09-27 13:59:47 +0200

    Add both forms to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,19 +12,26 @@ fi
 npm run build
 
 export AWS_PROFILE=pcivault
+
+echo "--- UPLOADING PCD FORM ---"
 aws s3 cp public/build/pcd_form.js s3://pvs-frontend/"$stage"/pcd/ --acl public-read
 aws s3 cp public/build/pcd_form.css s3://pvs-frontend/"$stage"/pcd/ --acl public-read
-aws s3 cp public/card.svg s3://pvs-frontend/"$stage"/pcd/ --acl public-read
-aws s3 cp public/how-to.html s3://pvs-frontend/"$stage"/pcd/how-to-capture-and-tokenize-payment-card-data.html --acl public-read
-aws s3 cp public/high-summit.regular.ttf s3://pvs-frontend/"$stage"/pcd/ --acl public-read
-aws s3 cp public/SFProDisplay-Bold.woff2 s3://pvs-frontend/"$stage"/pcd/ --acl public-read
-aws s3 cp public/SFProDisplay-Light.woff2 s3://pvs-frontend/"$stage"/pcd/ --acl public-read
-aws s3 cp public/SFProDisplay-Medium.woff2 s3://pvs-frontend/"$stage"/pcd/ --acl public-read
-aws s3 cp public/SFProDisplay-Regular.woff2 s3://pvs-frontend/"$stage"/pcd/ --acl public-read
-aws s3 cp public/logo.webp s3://pvs-frontend/"$stage"/pcd/ --acl public-read
-aws s3 cp public/chip.svg s3://pvs-frontend/"$stage"/pcd/ --acl public-read
+
+echo "--- UPLOADING ACH FORM ---"
+aws s3 cp public/build/ach_form.js s3://pvs-frontend/"$stage"/ach/ --acl public-read
+aws s3 cp public/build/ach_form.css s3://pvs-frontend/"$stage"/ach/ --acl public-read
+
+echo "--- UPLOADING SHARED ASSETS ---"
+aws s3 cp public/card.svg s3://pvs-frontend/"$stage"/ --acl public-read
+aws s3 cp public/high-summit.regular.ttf s3://pvs-frontend/"$stage"/ --acl public-read
+aws s3 cp public/SFProDisplay-Bold.woff2 s3://pvs-frontend/"$stage"/ --acl public-read
+aws s3 cp public/SFProDisplay-Light.woff2 s3://pvs-frontend/"$stage"/ --acl public-read
+aws s3 cp public/SFProDisplay-Medium.woff2 s3://pvs-frontend/"$stage"/ --acl public-read
+aws s3 cp public/SFProDisplay-Regular.woff2 s3://pvs-frontend/"$stage"/ --acl public-read
+aws s3 cp public/logo.webp s3://pvs-frontend/"$stage"/ --acl public-read
+aws s3 cp public/chip.svg s3://pvs-frontend/"$stage"/ --acl public-read
 
 for file in public/card-icons/*
 do
-  aws s3 cp "$file" s3://pvs-frontend/"$stage"/pcd/card-icons/ --acl public-read
+  aws s3 cp "$file" s3://pvs-frontend/"$stage"/card-icons/ --acl public-read
 done
