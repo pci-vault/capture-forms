bf0be447aaceebd8ddf478d9f8deac3a14ed1ea5
Ilian <ilian@teamgeek.io>
2026-06-25 09:26:40 +0200

    chore: clean up unused functions

--- src/ach_form.svelte
+++ src/ach_form.svelte
@@ -2,16 +2,12 @@
     import ACHForm from "./ACH/ACHForm.svelte";
 
     export let settings = {};
-    export let onSubmit;
-    export let onReset;
 </script>
 
 <main>
   <ACHForm
     pci_address_prod={process.env.pci_address_prod}
     pci_address_testing={process.env.pci_address_testing}
-    bind:submit={onSubmit}
-    bind:reset={onReset}
     {...settings}
   />
 </main>
--- src/pcd_form.svelte
+++ src/pcd_form.svelte
@@ -2,16 +2,12 @@
     import CreditCardForm from "./CreditCard/CreditCardForm.svelte";
 
     export let settings = {};
-    export let onSubmit;
-    export let onReset;
 </script>
 
 <main>
   <CreditCardForm
     pci_address_prod={process.env.pci_address_prod}
     pci_address_testing={process.env.pci_address_testing}
-    bind:submit={onSubmit}
-    bind:reset={onReset}
     {...settings}
   />
 </main>
