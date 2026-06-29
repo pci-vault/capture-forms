bdd606e3d334462b0cf79a948fe5881e935c99c7
Ilian <ilian@teamgeek.io>
2026-06-29 11:02:19 +0200

    fix: event listener removal on ACH form

--- src/ACH/ACHForm.svelte
+++ src/ACH/ACHForm.svelte
@@ -169,11 +169,11 @@
     window.removeEventListener("message", handleActionMessage);
   });
 
-  function focusOnFirstElement() {
+  const focusOnFirstElement = () => {
     document.getElementById("routing_number").focus();
   }
 
-  function handleActionMessage(event) {
+  const handleActionMessage = (event) => {
     if (event.data?.type === "action" && event.data?.name === "submit") {
       submit();
     } else if (event.data?.type === "action" && event.data?.name === "reset") {
